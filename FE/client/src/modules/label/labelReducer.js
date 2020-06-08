b376f0fa79060f89663ffa8956746b3fdc708858
최장호 <hmchoi651@naver.com>
2020-06-09 01:48:50 +0900

    [#5] feat: labelReducer : Test용 mock data 추가

--- FE/client/src/modules/label/labelReducer.js
+++ FE/client/src/modules/label/labelReducer.js
@@ -1,7 +1,13 @@
 import { ADD_LABEL, DELETE_LABEL } from "./labelAction";
 
 const initialState = {
-  labels: ["test"],
+  labels: [
+    { textColor: "#fff", backgroundColor: "#1E90FF", description: "testing label", labelName: "duplicate" },
+    { textColor: "#fff", backgroundColor: "#80E12A", description: "testing label", labelName: "FE" },
+    { textColor: "#fff", backgroundColor: "#52E4DC", description: "testing label", labelName: "good first issue" },
+    { textColor: "#fff", backgroundColor: "#FFACB7", description: "testing label", labelName: "help wanted" },
+    { textColor: "#fff", backgroundColor: "#FFB400", description: "testing label", labelName: "question" },
+  ],
 };
 
 const labelReducer = (state = initialState, action) => {
