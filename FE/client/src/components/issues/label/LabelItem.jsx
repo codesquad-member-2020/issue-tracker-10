8e3a4d380d93de7b3b6b7a7884a5fca00ea821eb
최장호 <hmchoi651@naver.com>
2020-06-09 01:50:16 +0900

    [#5] feat: LabelItem Detail content의 기본 Style 정의 및 Props로 받는 값 일부 추가

--- FE/client/src/components/issues/label/LabelItem.jsx
+++ FE/client/src/components/issues/label/LabelItem.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import { TableItem } from "@style/CustomStyle";
+import styled from "styled-components";
 
-const LabelItem = () => {
-  return <TableItem>_test</TableItem>;
+const LabelItem = ({ textColor, backgroundColor, description, MutedLink, labelName }) => {
+  return (
+    <TableItem>
+      <LabelTab>
+        <Label textColor={textColor} backgroundColor={backgroundColor}>
+          {labelName}
+        </Label>
+      </LabelTab>
+      <DescriptionTab>{description && description}</DescriptionTab>
+      <MutedLinkTab>{MutedLink && MutedLink}</MutedLinkTab>
+      <ButtonTab></ButtonTab>
+    </TableItem>
+  );
 };
+
+const LabelTab = styled.div`
+  width: 25%;
+`;
+
+const Label = styled.div`
+  display: inline-block;
+  box-sizing: border-box;
+  font-weight: bold;
+  font-size: 14px;
+  padding: 4px 8px;
+  border-radius: 3px;
+  color: ${(props) => `${props.textColor}`};
+  background-color: ${(props) => `${props.backgroundColor}`};
+`;
+
+const DescriptionTab = styled.div`
+  font-size: 12px;
+  color: #586069 !important;
+  width: 33.3333%;
+`;
+
+const MutedLinkTab = styled.div`
+  font-size: 12px;
+  color: #586069 !important;
+  width: 25%;
+`;
+
+const ButtonTab = styled.div`
+  font-size: 12px;
+  color: #586069 !important;
+  width: 16.66667%;
+`;
 
 export default LabelItem;
