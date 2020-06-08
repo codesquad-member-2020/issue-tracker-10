d1311c1b2f0ceae32b9324ba8ee3cc10010f553b
최장호 <hmchoi651@naver.com>
2020-06-08 21:30:12 +0900

    [#5] feat: 전역에 적용하는 기본 Style 속성 추가

---
+++ FE/client/src/style/GlobalStyle.jsx
@@ -0,0 +1,20 @@
+import { createGlobalStyle, css } from "styled-components";
+import reset from "styled-reset";
+
+const variables = css``;
+
+export const GlobalStyle = createGlobalStyle`
+${reset}
+${variables}
+
+* {
+    font-size: 1rem;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+
+`;
