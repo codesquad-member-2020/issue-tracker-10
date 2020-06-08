6043d8021d9a625a4cfc1896c2022cfaa17edde6
최장호 <hmchoi651@naver.com>
2020-06-08 23:41:57 +0900

    [#5] feat: Table 관련 재사용 가능한 Styles를 CustomStyle에 정의 (TableWrap , TableTopMenu , TableItem)

---
+++ FE/client/src/style/CustomStyle.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const TableWrap = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid #d1d5da;
+  border-radius: 3px;
+  background-color: #fff;
+  width: 980px;
+`;
+
+export const TableTopMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  background-color: #f6f8fa;
+  margin: -1px -1px 0;
+  padding: 16px;
+  border: 1px solid #d1d5da;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  font-size: 14px;
+`;
+
+export const TableItem = styled.div`
+  width: 100%;
+  padding: 16px;
+  background-color: #fff;
+  border: 1px solid #d1d5da;
+  border-top: none;
+`;
