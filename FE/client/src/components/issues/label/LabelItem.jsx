d0705fffa11d7c66d2c683c12781700324ff2723
최장호 <hmchoi651@naver.com>
2020-06-10 00:41:34 +0900

    [#5] feat: LabelItem 상태관리 변경 및 Event 추가
    - props로 받은 Server의 InitData를 formatState와 snapshot State로 관리
    - onClickEdit 이벤트 추가
    - Edit이 Open일 때 Label 정보 조건부 랜더링으로 변경
    - 중복 Style 제거
    - Edit Component 추가

--- FE/client/src/components/issues/label/LabelItem.jsx
+++ FE/client/src/components/issues/label/LabelItem.jsx
@@ -1,39 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { TableItem } from "@style/CustomStyle";
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { deleteLabel } from "@modules/label/labelAction";
+import Edit from "./Edit";
 
 const DELETE_CONFIRM_MESSAGE = '"Are you sure? Deleting a label will remove it from all issues and pull requests."';
 
-const LabelItem = ({ id, textColor, backgroundColor, description, MutedLink, labelName }) => {
+const LabelItem = (props) => {
   const dispatch = useDispatch();
+  const [editIsOpen, setEditIsOpen] = useState(false);
 
-  const onClickEdit = () => {};
+  const [format, setFormat] = useState({ ...props });
+  const [snapshot, setSnapShot] = useState({ ...props });
+
+  const { id, textColor, backgroundColor, description, MutedLink, labelName } = format;
+
+  const onClickEdit = () => {
+    setEditIsOpen(!editIsOpen);
+  };
 
   const onClickDelete = () => {
     if (window.confirm(DELETE_CONFIRM_MESSAGE)) dispatch(deleteLabel(id));
   };
 
   return (
-    <TableItem>
-      <LabelTab>
-        <Label textColor={textColor} backgroundColor={backgroundColor}>
-          {labelName}
-        </Label>
-      </LabelTab>
-      <DescriptionTab>{description && description}</DescriptionTab>
-      <MutedLinkTab>{MutedLink && MutedLink}</MutedLinkTab>
-      <ButtonTab>
-        <Button>Edit</Button>
-        <Button onClick={onClickDelete}>Delete</Button>
-      </ButtonTab>
-    </TableItem>
+    <TableItemLabel>
+      <Info>
+        <LabelTab>
+          <Label textColor={textColor} backgroundColor={backgroundColor}>
+            {labelName}
+          </Label>
+        </LabelTab>
+        <DescriptionTab>{!editIsOpen && description}</DescriptionTab>
+        <MutedLinkTab>{!editIsOpen && MutedLink}</MutedLinkTab>
+        <ButtonTab>
+          <Button onClick={onClickEdit}>Edit</Button>
+          <Button onClick={onClickDelete}>Delete</Button>
+        </ButtonTab>
+      </Info>
+      {editIsOpen && <Edit format={format} setFormat={setFormat} snapshot={snapshot} setSnapShot={setSnapShot} />}
+    </TableItemLabel>
   );
 };
 
+const TableItemLabel = styled(TableItem)`
+  flex-direction: column;
+`;
+
 const LabelTab = styled.div`
   width: 25%;
+`;
+
+const Info = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
 `;
 
 const Label = styled.div`
@@ -45,26 +67,19 @@ const Label = styled.div`
   border-radius: 3px;
   color: ${(props) => `${props.textColor}`};
   background-color: ${(props) => `${props.backgroundColor}`};
-  line-height: 1.25;
 `;
 
 const DescriptionTab = styled.div`
-  font-size: 12px;
-  color: #586069 !important;
   width: 33.3333%;
 `;
 
 const MutedLinkTab = styled.div`
-  font-size: 12px;
-  color: #586069 !important;
   width: 25%;
 `;
 
 const ButtonTab = styled.div`
   display: flex;
   justify-content: flex-end;
-  font-size: 12px;
-  color: #586069 !important;
   width: 16.66667%;
 `;
 
