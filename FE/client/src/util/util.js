3ec65003addd3add92dad76aa068529594e99cd5
최장호 <hmchoi651@naver.com>
2020-06-09 18:36:41 +0900

    [#5] feat: util 폴더 추가
    debounce : input의 입력을 지연처리 시키는 deboune 함수 추가
    pipe : 함수 합성을 위한 pipeline 함수 추가
    getRandomHexColor : 랜덤한 16진수 컬러를 만드는 함수
    hexToRgb : 16진수를 rgb 색상으로 변경하는 코드 함수
    isDarkColor : rgb 색상을 기준으로 Text의 색상을 정하는 함수

---
+++ FE/client/src/util/util.js
@@ -0,0 +1,37 @@
+const _ = {
+  debounce: (func) => {
+    let debounce;
+
+    clearTimeout(debounce);
+
+    debounce = setTimeout(() => {
+      func();
+    }, 1000);
+  },
+
+  pipe: (...functions) => (args) => functions.reduce((arg, nextFn) => nextFn(arg), args),
+
+  createRandomHexColor: () => {
+    const randomHexColor = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+
+    return randomHexColor;
+  },
+
+  changeHexToRgb: (hex) => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16),
+        }
+      : null;
+  },
+
+  isDarkColorText: ({ r, g, b }) => {
+    const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+    return yiq < 150;
+  },
+};
+
+export default _;
