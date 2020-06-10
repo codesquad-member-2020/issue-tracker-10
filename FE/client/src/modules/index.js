c121eaddccf43c9b4132ef0d19bb0eddce0c0d73
sgdg <seungdeng17@gmail.com>
2020-06-10 14:41:52 +0900

    [#10] feat: 엔트리 포인트 내용 추가

--- FE/client/src/modules/index.js
+++ FE/client/src/modules/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import label from "./label/labelReducer";
+import milestones from "./milestones";
 
-const rootReducers = combineReducers({ label });
+const rootReducers = combineReducers({
+    label,
+    milestones,
+});
 
 export default rootReducers;
