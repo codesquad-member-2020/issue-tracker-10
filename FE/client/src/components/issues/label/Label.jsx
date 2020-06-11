cd2ecc01e06cb382d7bc13ee80858189d95aa5e4
최장호 <hmchoi651@naver.com>
2020-06-11 16:52:46 +0900

    [#5] feat: Create Label 기본 위치 지정

--- FE/client/src/components/issues/label/Label.jsx
+++ FE/client/src/components/issues/label/Label.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { TableHeaderButton } from "@style/CustomStyle";
+import { TableHeaderButton, LabelBox } from "@style/CustomStyle";
 
 import Table from "@component/table/Table";
 import LabelTopMenu from "./LabelTopMenu";
 import LabelItem from "./LabelItem";
 import TableHeader from "@component/table/TableHeader";
 import SwitchButtons from "@component/table/SwitchButtons";
+import LabelEditor from "./LabelEditor";
 
 import { useSelector } from "react-redux";
+
+import { CREATE_LABEL_INFO } from "./labelConstanst";
 
 const LABEL_TEXT = "New Label";
 
 const Label = () => {
   const { labels } = useSelector((state) => state.labels);
+  const [createIsOpen, setCreateIsOpen] = useState(false);
+
+  const [format, setFormat] = useState({ ...CREATE_LABEL_INFO });
+  const [snapshot, setSnapShot] = useState({ ...CREATE_LABEL_INFO });
+
   const rightSideComponent = <TableHeaderButton>{LABEL_TEXT}</TableHeaderButton>;
   const leftSideComponent = <SwitchButtons type="labels" />;
 
@@ -21,9 +29,19 @@ const Label = () => {
     return <LabelItem key={idx} {...labelOption} />;
   });
 
+  const onClickEdit = () => setCreateIsOpen(!createIsOpen);
+
   return (
     <>
       <TableHeader leftSideComponent={leftSideComponent} rightSideComponent={rightSideComponent} />
+      <CreateLabelWrap>
+        <CreateLabelInner>
+          <LabelBox backgroundColor={format.backgroundColor} textColor={format.textColor}>
+            {format.labelName}
+          </LabelBox>
+          <LabelEditor type="Create" format={format} setFormat={setFormat} snapshot={snapshot} setSnapShot={setSnapShot} onClickEdit={onClickEdit} />
+        </CreateLabelInner>
+      </CreateLabelWrap>
       <LabelWrap>
         <Table renderTableTopMenu={<LabelTopMenu />} renderTableList={_test_items} />
       </LabelWrap>
@@ -40,6 +58,20 @@ const LabelWrap = styled.div`
   background-color: #fff;
   font-size: 12px;
   color: #586069;
+`;
+
+const CreateLabelWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const CreateLabelInner = styled.div`
+  width: 980px;
+  border: 1px solid #e1e4e8;
+  background-color: #f6f8fa;
+  margin-bottom: 16px;
+  padding: 16px;
 `;
 
 export default Label;
