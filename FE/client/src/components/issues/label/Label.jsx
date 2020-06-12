c86a05a840d47b0c1826cd392045f13317dc5eba
최장호 <hmchoi651@naver.com>
2020-06-12 13:08:50 +0900

    [#5] feat: InfoMessage Component 재사용 가능하게 Title과 Content를 인자로 받는 형식으로 리팩토링

--- FE/client/src/components/issues/label/Label.jsx
+++ FE/client/src/components/issues/label/Label.jsx
@@ -13,7 +13,7 @@ import InfoMessage from "@components/infoMessage/InfoMessage";
 import { useSelector, useDispatch } from "react-redux";
 import { addLabel } from "@modules/labels";
 
-import { CREATE_LABEL_INFO } from "./labelConstant";
+import { CREATE_LABEL_INFO, NO_LABEL_TITLE, NO_LABEL_CONTENT } from "./labelConstant";
 
 const LABEL_TEXT = "New Label";
 
@@ -59,7 +59,7 @@ const Label = () => {
         </CreateLabelWrap>
       )}
       <LabelWrap>
-        <Table renderTableTopMenu={<LabelTopMenu />} renderTableList={_test_items.length ? _test_items : <InfoMessage />} />
+        <Table renderTableTopMenu={<LabelTopMenu />} renderTableList={_test_items.length ? _test_items : <InfoMessage title={NO_LABEL_TITLE} content={NO_LABEL_CONTENT} />} />
       </LabelWrap>
     </>
   );
