b23312050cbe0117f2285a79a6da69105e06fdab
최장호 <hmchoi651@naver.com>
2020-06-14 20:54:27 +0900

    [#27] style: GlobalStyle 속성 추가

--- FE/client/src/style/GlobalStyle.jsx
+++ FE/client/src/style/GlobalStyle.jsx
@@ -27,4 +27,31 @@ ${variables}
   background: transparent;
  }
 
+ input {
+    width: 100%;
+    padding: 6px 8px;
+    font-size: 16px;
+    border: 1px solid #d1d5da;
+    border-radius: 3px;
+    outline: none;
+    box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075);
+    background-color: #fafbfc;
+    min-height: 32px;
+    :focus {
+      background-color: #fff;
+      border-color: #2188ff;
+      outline: none;
+      box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075), 0 0 0 0.2em rgba(3, 102, 214, 0.3);
+    }
+  }
+ textarea {
+    :focus {
+      background-color: #fff;
+      border-color: #2188ff;
+      outline: none;
+      box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075), 0 0 0 0.2em rgba(3, 102, 214, 0.3);
+    }
+ }
+
+
 `;
