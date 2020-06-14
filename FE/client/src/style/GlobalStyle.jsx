0fc7dc0868619a11da334e727a43bcaa78e5f1b6
최장호 <hmchoi651@naver.com>
2020-06-14 23:59:43 +0900

    [#27] style: GlobalStyle Button 속성 추가

--- FE/client/src/style/GlobalStyle.jsx
+++ FE/client/src/style/GlobalStyle.jsx
@@ -25,6 +25,13 @@ ${variables}
   padding: 0;
   width: auto;
   background: transparent;
+   height: 32px;
+    border-radius: 3px;
+    padding: 7px 12px;
+    font-size: 14px;
+    font-weight: 600;
+    border: 1px solid rgba(27, 31, 35, 0.2);
+    outline: none;
  }
 
  input {
