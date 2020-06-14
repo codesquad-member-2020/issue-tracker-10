ebe469725caeddb9dd19c8ec3c66033ca6c94e0d
최장호 <hmchoi651@naver.com>
2020-06-15 00:00:16 +0900

    [#27] feat: IssuesEditor 폴더에 관한 상수 파일 분류

---
+++ FE/client/src/components/issues/issuesEditor/issyesEditorConstant.js
@@ -0,0 +1,3 @@
+export const MARKDOWN_INFO_MESSGAE = "Supports Markdown grammar.";
+export const MARKDOWN_WIKI_URL = "https://ko.wikipedia.org/wiki/%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4";
+export const SUBMIT_NEW_ISSUE = "Submit new issue";
