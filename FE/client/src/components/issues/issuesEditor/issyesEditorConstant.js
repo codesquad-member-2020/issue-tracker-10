0216fd1002c0211668865724ae0fdb9aa5b82304
최장호 <hmchoi651@naver.com>
2020-06-15 19:04:20 +0900

    [#27] docs: Error message Constant 추가

--- FE/client/src/components/issues/issuesEditor/issyesEditorConstant.js
+++ FE/client/src/components/issues/issuesEditor/issyesEditorConstant.js
@@ -1,3 +1,4 @@
 export const MARKDOWN_INFO_MESSGAE = "Supports Markdown grammar.";
 export const MARKDOWN_WIKI_URL = "https://ko.wikipedia.org/wiki/%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4";
 export const SUBMIT_NEW_ISSUE = "Submit new issue";
+export const ISSUES_TITLE_ERROR_MESSAGE = "Title value is required. And Title long is (maximum is 256 characters.)  ";
