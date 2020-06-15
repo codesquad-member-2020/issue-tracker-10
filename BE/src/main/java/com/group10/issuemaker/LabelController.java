9bac3eddabb419a58704f4d278d3323d7aa80369
Jenny <huji3156@gmail.com>
2020-06-15 16:48:49 +0900

    chore : 코드 정리

--- BE/src/main/java/com/group10/issuemaker/LabelController.java
+++ BE/src/main/java/com/group10/issuemaker/LabelController.java
@@ -1,11 +1,8 @@
 package com.group10.issuemaker;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
 import javax.sql.DataSource;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class LabelController {
@@ -17,18 +14,21 @@ public class LabelController {
     }
 
 //    @PostMapping(value = "/labels/create", consumes = MediaType.APPLICATION_JSON_VALUE)
-@PostMapping(value = "/labels/create")
-public List<Label> makeNewLabel(@RequestBody Map<String, String> label) {
-
-
-        labelDAO.createLabel(label.get("textColor"), label.get("backgroundColor"), label.get("description"), label.get("labelName"));
-//        labelDAO.createLabel("black", "blue", "jeju-coding", "will be awesome");
+    @PostMapping(value = "/labels/create")
+    public List<Label> makeNewLabel(@RequestBody Label label) {
+        labelDAO.createLabel(label.getTextColor(), label.getBackgroundColor(), label.getDescription(), label.getLabelName());
         return labelDAO.findLabels();
     }
 
-    @GetMapping("/labels/{labelId}/delete")
+    @DeleteMapping("/labels/{labelId}/delete")
     public List<Label> deleteLabel(@PathVariable Long labelId) {
         labelDAO.deleteLabel(labelId);
+        return labelDAO.findLabels();
+    }
+
+    @PostMapping("/labels/{labelId}/edit")
+    public List<Label> editLabel(@RequestBody Label label, @PathVariable Long labelId) {
+        labelDAO.editLabel(labelId,label.getLabelName(), label.getDescription(), label.getTextColor());
         return labelDAO.findLabels();
     }
 }
