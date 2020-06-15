4260f0f2fc1320b2573b3d1aee802dbdb993c3f9
최장호 <hmchoi651@naver.com>
2020-06-15 22:59:36 +0900

    [#27] feat: GithubPicker 파일명 변경

--- FE/client/src/components/issues/issuesEditor/GithubPicker_.jsx
+++ FE/client/src/components/issues/issuesEditor/GithubPicker_.jsx
@@ -119,7 +119,7 @@ const useStyles = makeStyles((theme) => ({
 export default function GitHubLabel() {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const [value, setValue] = React.useState([labels[1], labels[11]]);
+  const [value, setValue] = React.useState([]);
   const [pendingValue, setPendingValue] = React.useState([]);
   const theme = useTheme();
 
@@ -220,85 +220,5 @@ const labels = [
     name: "help wanted",
     color: "#008672",
     description: "Extra attention is needed",
-  },
-  {
-    name: "priority: critical",
-    color: "#b60205",
-    description: "",
-  },
-  {
-    name: "priority: high",
-    color: "#d93f0b",
-    description: "",
-  },
-  {
-    name: "priority: low",
-    color: "#0e8a16",
-    description: "",
-  },
-  {
-    name: "priority: medium",
-    color: "#fbca04",
-    description: "",
-  },
-  {
-    name: "status: can't reproduce",
-    color: "#fec1c1",
-    description: "",
-  },
-  {
-    name: "status: confirmed",
-    color: "#215cea",
-    description: "",
-  },
-  {
-    name: "status: duplicate",
-    color: "#cfd3d7",
-    description: "This issue or pull request already exists",
-  },
-  {
-    name: "status: needs information",
-    color: "#fef2c0",
-    description: "",
-  },
-  {
-    name: "status: wont do/fix",
-    color: "#eeeeee",
-    description: "This will not be worked on",
-  },
-  {
-    name: "type: bug",
-    color: "#d73a4a",
-    description: "Something isn't working",
-  },
-  {
-    name: "type: discussion",
-    color: "#d4c5f9",
-    description: "",
-  },
-  {
-    name: "type: documentation",
-    color: "#006b75",
-    description: "",
-  },
-  {
-    name: "type: enhancement",
-    color: "#84b6eb",
-    description: "",
-  },
-  {
-    name: "type: epic",
-    color: "#3e4b9e",
-    description: "A theme of work that contain sub-tasks",
-  },
-  {
-    name: "type: feature request",
-    color: "#fbca04",
-    description: "New feature or request",
-  },
-  {
-    name: "type: question",
-    color: "#d876e3",
-    description: "Further information is requested",
   },
 ];
