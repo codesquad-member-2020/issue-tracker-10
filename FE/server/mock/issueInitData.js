eef933a46a2bd58006b25812b2b232b6e6573629
sgdg <seungdeng17@gmail.com>
2020-06-15 23:00:51 +0900

    [#30] feat: issue 목록 화면 구현
    - 스타일 작업
    - 모달 추가 해야 함

--- FE/server/mock/issueInitData.js
+++ FE/server/mock/issueInitData.js
@@ -3,11 +3,13 @@ const issueList = {
         {
             "id": 1,
             "title": "목록 보기 구현",
+            "bOpen": true,
+            "createDate": "2020-06-11T12:48:28+09:00",
             "labels": [
                 {
                     "title": "bug",
                     "color": "#fff",
-                    "backgroundColor": "#f00",
+                    "backgroundColor": "rgb(118,148,231)",
                 }
             ],
             "milestones": {
@@ -22,17 +24,19 @@ const issueList = {
                 {
                     "id": "seungdeng17",
                     "url": "imageURL"
-                }
+                },
             ]
         },
         {
             "id": 2,
             "title": "이것은 영국으로 부터 시작 된",
+            "bOpen": true,
+            "createDate": "2020-06-11T12:48:28+09:00",
             "labels": [
                 {
                     "title": "FE",
-                    "color": "#000",
-                    "backgroundColor": "#ff0"
+                    "color": "#fff",
+                    "backgroundColor": "rgb(86,185,42)"
                 }
             ],
             "milestones": {
@@ -50,11 +54,13 @@ const issueList = {
         {
             "id": 3,
             "title": "[오피셜] 손흥민 레알마드리드 이적",
+            "bOpen": true,
+            "createDate": "2020-06-11T12:48:28+09:00",
             "labels": [
                 {
                     "title": "official",
-                    "color": "#000",
-                    "backgroundColor": "#fff"
+                    "color": "#fff",
+                    "backgroundColor": "rgb(254,40,119)"
                 }
             ],
             "milestones": {
