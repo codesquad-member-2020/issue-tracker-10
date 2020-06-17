b78c9bf1f158b81377120d59f7294ee78d370f06
최장호 <hmchoi651@naver.com>
2020-06-17 21:37:13 +0900

    [#27] feat: 선택된 아이템을 보여주는 Check icon 추가

--- FE/client/src/components/issues/issuesEditor/githubPicker/ColorPickerItem.jsx
+++ FE/client/src/components/issues/issuesEditor/githubPicker/ColorPickerItem.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+
 import styled from "styled-components";
+import { FiCheck } from "react-icons/fi";
 
 const ColorPickerItem = (props) => {
   const { backgroundColor, labelName, description } = props;
 
   return (
     <PickerListItem>
+      <PickerItemCheck>
+        <FiCheck />
+      </PickerItemCheck>
       <PickerItemLabel backgroundColor={backgroundColor} />
       <PickerItemInfo>
         <div>{labelName}</div>
@@ -21,6 +26,9 @@ const PickerListItem = styled.div`
   display: flex;
   padding: 8px;
   background-color: #fff;
+  :hover {
+    background-color: #f5f5f5;
+  }
 `;
 
 const PickerItemLabel = styled.div`
@@ -30,6 +38,14 @@ const PickerItemLabel = styled.div`
   margin-right: 8px;
   border-radius: 3px;
   margin-top: 4px;
+`;
+
+const PickerItemCheck = styled.div`
+  width: 17px;
+  height: 17px;
+  margin-left: -2px;
+  margin-right: 5px;
+  margin-top: 3px;
 `;
 
 const PickerItemInfo = styled.div``;
