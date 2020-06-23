8817dceaeba3eaed905a7a96073e5ca24cef2e6a
sgdg <seungdeng17@gmail.com>
2020-06-23 18:47:32 +0900

    [#64] feat: api url 추가

---
+++ FE/client/src/constants/url.js
@@ -0,0 +1,13 @@
+const DOMAIN_ROOT = process.env.REACT_APP_DOMAIN_ROOT;
+
+export const URL = {
+    MILESTONE_LIST_API: DOMAIN_ROOT + process.env.REACT_APP_MILESTONE_LIST_API,
+
+    LABEL_LIST_API: DOMAIN_ROOT + process.env.REACT_APP_LABEL_LIST_API,
+    LABEL_CREATE_API: DOMAIN_ROOT + process.env.REACT_APP_LABEL_CREATE_API,
+    LABEL_EDIT_API: (labelId) => DOMAIN_ROOT + process.env.REACT_APP_LABEL_EDIT_API.replace('{labelId}', labelId),
+
+    ISSUE_LIST_API: DOMAIN_ROOT + process.env.REACT_APP_ISSUE_LIST_API,
+    ISSUE_PICKER_INFO_API: DOMAIN_ROOT + process.env.REACT_APP_ISSUE_PICKER_INFO_API,
+    ISSUE_API: (issueId) => DOMAIN_ROOT + process.env.REACT_APP_ISSUE_API.replace('{issueId}', issueId),
+}
