559d2a37cef2d2973644fe4a036552b651f8cd55
Jenny <huji3156@gmail.com>
2020-06-25 15:28:39 +0900

    feat : data 추가 및 수정

--- BE/build/resources/main/data.sql
+++ BE/build/resources/main/data.sql
@@ -11,11 +11,18 @@ insert into issue (issue_id, title, content, opened_date, closed_date, opened, m
 insert into issue (issue_id, title, content, opened_date, closed_date, opened, milestone_id, author_id) values (3, 'eat healthy', 'eat organic stuff', '2020-06-11T12:48:28+09:00', 'tbd', 0, 2, 1);
 
 
-insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (1, '#fff', 'rgb(203,92,208)', 'testing label1', 'group 10 is the best');
-insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (2, '#fff', 'rgb(254,40,119)', 'testing label2', 'XP is beyond silver');
-insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (3, '#fff', 'rgb(86,185,42)', 'testing label3', 'Hoi is Korean tiger');
-insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (4, '#fff', 'rgb(118,148,231)', 'testing label4', 'Taek is TaekSin');
-insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (5, '#000', 'rgb(128,177,104)', 'testing label5', 'Ellie is Alligator');
+insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (1, '#fff', 'rgb(203,92,208)',  'testing label1', '코쿼 수강생 여러분');
+insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (2, '#fff', 'rgb(254,40,119)',  'testing label2', '한 학기동안 수고 많았어요');
+insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (3, '#fff', 'rgb(86,185,42)',   'testing label3', '겨울에 시작해 봄을 지나');
+insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (4, '#fff', 'rgb(118,148,231)', 'testing label4', '무더운 여름이 되었네요');
+insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (5, '#000', 'rgb(128,177,104)', 'testing label5', '모두 원하는 것을');
+insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (6, '#000', 'rgb(128,177,104)', 'testing label6', '이루길 바라요');
+insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (7, '#000', 'rgb(128,177,104)', 'testing label7', '힘든 과정을 견딜 수 있었던 것은 다 여러분 덕분이에요');
+insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (8, '#000', 'rgb(128,177,104)', 'testing label7', '우사인 볼트가 세상에서 가장 빠른 이유는');
+insert into label (label_id, textColor, backGroundColor, description, labelName) VALUES (9, '#000', 'rgb(128,177,104)', 'testing label7', '끝까지 갔기 때문이에요');
+
+
+
 
 insert into issue_label (issue_id, label_id) VALUES (1, 1);
 insert into issue_label (issue_id, label_id) VALUES (1, 2);
